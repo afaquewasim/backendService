502186ae9b1c8613fdc3effda7aa1b0f0a86be87
Afaque Wasim <afaquewasim786@gmail.com>
2024-03-20 15:37:24 +0530

    User Created

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -1,9 +1,56 @@
-import {asyncHandller} from '../utils/asyncHandller.js'
+import { asyncHandller } from '../utils/asyncHandller.js'
+import { ApiError } from '../utils/ApiErrors.js'
+import { User } from '../models/user.models.js'
+import { uploadOnCloudinary } from '../utils/cloudinary.js'
+import { ApiResponse } from '../utils/ApiResponse.js'
 
-const registerUser = asyncHandller( async (req, res) => {
-    res.status(200).json({
-        message: "OK"
+const registerUser = asyncHandller(async (req, res) => {
+    const { fullname, username, email, password } = req.body
+    console.log("email", email);
+
+    if ([fullname, username, email, password].some((field) => field?.trim() === "")) {
+        throw new ApiError(400, "All field are required")
+    }
+    const existedUser = User.findOne({
+        $or: [{ username }, { email }]
     })
+
+    if (existedUser) {
+        throw new ApiError(409, "User already exists")
+    }
+
+    const avatarLocalPath = req.files?.avatar[0].path
+    const coverImageLocalPath = req.files?.coverImage[0].path
+
+    if (!avatarLocalPath) {
+        throw new ApiError(400, "Avatar is required")
+    }
+
+    const avatar = await uploadOnCloudinary(avatarLocalPath)
+    const coverImage = await uploadOnCloudinary(coverImageLocalPath)
+
+    if (avatar) {
+        throw new ApiError(400, "Avatar is required")
+    }
+
+    const user = await User.create({
+        email,
+        fullname,
+        password,
+        username: username.toLowerCase(),
+        avatar: avatar.url,
+        coverImage: coverImage?.url || ""
+    })
+
+    const createdUser = User.findById(user._id).select("-password -refreshToken")
+
+    if (!createdUser) {
+        throw new ApiError(500, "Something went wrong while registring the user")
+    }
+
+    return res.status(201).json(
+        new ApiResponse(200, createdUser, "Registered sucessfully")
+    )
 })
 
-export {registerUser}
+export { registerUser }
