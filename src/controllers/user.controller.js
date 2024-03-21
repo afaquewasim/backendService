0586ed10f19433d435e728b2081ba9f138fd680c
Afaque Wasim <afaquewasim786@gmail.com>
2024-03-22 02:06:11 +0530

    user Register

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -3,6 +3,7 @@ import { ApiError } from '../utils/ApiErrors.js'
 import { User } from '../models/user.models.js'
 import { uploadOnCloudinary } from '../utils/cloudinary.js'
 import { ApiResponse } from '../utils/ApiResponse.js'
+import router from '../routes/user.routes.js'
 
 const registerUser = asyncHandller(async (req, res) => {
     const { fullname, username, email, password } = req.body
@@ -11,7 +12,7 @@ const registerUser = asyncHandller(async (req, res) => {
     if ([fullname, username, email, password].some((field) => field?.trim() === "")) {
         throw new ApiError(400, "All field are required")
     }
-    const existedUser = User.findOne({
+    const existedUser = await User.findOne({
         $or: [{ username }, { email }]
     })
 
@@ -20,7 +21,13 @@ const registerUser = asyncHandller(async (req, res) => {
     }
 
     const avatarLocalPath = req.files?.avatar[0].path
-    const coverImageLocalPath = req.files?.coverImage[0].path
+    // const coverImageLocalPath = req.files?.coverImage[0].path
+
+    let coverImageLocalPath
+
+    if (req.files && Array.isArray(req.files.coverImage && req.files.coverImage.length > 0)) {
+        coverImageLocalPath = req.files.coverImage[0].path
+    }
 
     if (!avatarLocalPath) {
         throw new ApiError(400, "Avatar is required")
@@ -29,7 +36,7 @@ const registerUser = asyncHandller(async (req, res) => {
     const avatar = await uploadOnCloudinary(avatarLocalPath)
     const coverImage = await uploadOnCloudinary(coverImageLocalPath)
 
-    if (avatar) {
+    if (!avatar) {
         throw new ApiError(400, "Avatar is required")
     }
 
@@ -38,7 +45,7 @@ const registerUser = asyncHandller(async (req, res) => {
         fullname,
         password,
         username: username.toLowerCase(),
-        avatar: avatar.url,
+        avatar: avatar ? avatar.url : null,
         coverImage: coverImage?.url || ""
     })
 
